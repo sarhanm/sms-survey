6733f96e323f0a66afe7bffb3199c3475cfd4408
Mohammad Sarhan <mohammad@zillow.com>
2014-04-15 18:09:29 -0700

    Importing with composer installation in mind

--- src/import.php
+++ src/import.php
@@ -2,9 +2,24 @@
 /**
  * Base file to import the vendor/autoload.php file
  */
-# Autoloads all dependencies. Including our classes
+// Autoloads all dependencies. Including our classes
+// Required for the logj4 configuration.
+// TODO: Need to change this so its not hard coded
 date_default_timezone_set('America/Los_Angeles');
-require_once __DIR__."/../vendor/autoload.php";
+
+$autoload = __DIR__."/../vendor/autoload.php";
+
+if(!file_exists($autoload))
+{
+    //Must be installed via composer. Need to go out a few more dirs
+    $autoload = __DIR__.'/../../../autoload.php';
+}
+
+require_once $autoload;
+
 Logger::configure(__DIR__."/../config/log-config.xml");
+
 define('SARHAN_SURVEY',true);
+
+unset($autoload);
 ?>
