b01da5ffc6d75747a46742641f84133c70385b2d
Mohammad Sarhan <mohammad@zillow.com>
2015-04-26 10:10:06 -0700

    Adding helperset to globals to allow for different ways to import our project.

--- config/cli-config.php
+++ config/cli-config.php
@@ -9,5 +9,7 @@
 require_once __DIR__."/../src/import.php";
 
 $entityManager = sarhan\survey\SurveyEntityManager::getEntityManager();
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+$helperSet =  \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+$GLOBALS['doctrine_survey_helperset'] = $helperSet;
+return $helperSet;
 ?>
